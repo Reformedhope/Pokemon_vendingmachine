f644d8c26e1fa11ecfe9d433d855193fd5fe06ad
reformedhope <steph_salzburg@hotmail.com>
2023-09-03 20:14:29 -0400

    added Pokemon classes and pokemon buffs

---
+++ Pokemon.py
@@ -0,0 +1,50 @@
+
+
+class Pikachu:
+    def __init__(self,name,cost):
+        self.name = name
+        self.power = 55
+        self.health = 100
+
+
+
+
+class Charizard:
+    def __init__(self,name,cost):
+        self.name = name
+        self.power = 70
+        self.health = 100
+        
+
+
+class Blastoise:
+    def __init__(self,name,cost):
+        self.name = name
+        self.power = 70
+        self.health = 100
+        
+
+
+
+class Venusaur:
+    def __init__(self,name,cost):
+        self.name = name
+        self.power = 70
+        self.health = 100
+        
+
+
+class Ninetails:
+    def __init__(self,name,cost):
+        self.name = name
+        self.power = 60
+        self.health = 100
+    
+
+
+class Arcaine:
+    def __init__(self,name,cost):
+        self.name = name
+        self.power = 55
+        self.health = 100
+        
---
+++ vending.py
@@ -0,0 +1,17 @@
+
+
+def user_funds_amount(self):
+    user_coins = int(input("Please enter the amount of money you wish you pay for a pokemon "))
+    print(user_coins)
+
+def vending_input(self):
+    
+    while user_funds_amount >= 50 and user_funds_amount <= 49:
+        
+
+
+
+
+def vending_transaction(self):
+    self.user_funds_amount()
+
